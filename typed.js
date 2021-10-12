4fd01efa86ead661e394273b067af8fcaca51854
Johan Puentes <puentespulidojohansebastian@gmail.com>
2021-10-11 23:46:22 -0500

    first commit

---
+++ typed.js
@@ -0,0 +1,10 @@
+import './style.css';
+import Typed from 'typed.js';
+
+var options = {
+    strings: ['Developer','Designer','Enginer','Student'],
+    typeSpeed: 50,
+    loop: true
+}
+
+var typed  = new  Typed('.typed', options);
